b0420d4a705590d0a835d831b1d6fdfcb39434ed
Rett Berg <rett@google.com>
2020-04-21 15:33:38 -0600

    start grammar ground-up

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,36 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+mod parser;
+
+use std::fs;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "example", about = "An example of StructOpt usage.")]
+#[structopt(name = "lang", about = "An experimental programming language.")]
 struct Opt {
     /// Input file
     #[structopt(parse(from_os_str))]
     input: PathBuf,
 
     /// Output file
-    #[structopt(parse(from_os_str))]
+    #[structopt(parse(from_os_str), default_value = "out.wat")]
     output: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     env_logger::init();
     let opt = Opt::from_args();
     info!("{:?}", opt);
+    let text = fs::read_to_string(&opt.input)
+        .map_err(|e| format!("Failed to parse {}: {}", opt.input.display(), e))?;
+    let res = match parser::parse(&text) {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("Failed to parse {}\n{:#?}", opt.input.display(), e);
+            return Err(e.to_string());
+        }
+    };
+    Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,13 @@
+use pest;
+use pest::iterators::Pair;
+use pest::Parser;
+
 #[derive(Parser)]
 #[grammar = "syntax.pest"]
-pub struct Parser;
+pub struct LangParser;
+
+pub fn parse(text: &str) -> Result<(), pest::error::Error<Rule>> {
+    LangParser::parse(Rule::file, text)?;
+
+    Ok(())
+}
