f8d45d8f8fd0d926fe5da1cb0de749fde08c0b4d
Rett Berg <rett@google.com>
2020-04-21 22:43:01 -0600

    almost done

--- src/parser.rs
+++ src/parser.rs
@@ -43,22 +43,27 @@ pub fn build_ast<'a>(
 
     for pair in pairs {
         match pair.as_rule() {
-            Rule::declare_fn => {
-                functions.push(parse_fn(path, pair)?);
-            }
+            Rule::declare_fn => functions.push(parse_declare_fn(path, pair)?),
             Rule::declare_var => {}
             Rule::EOI => {}
-            _ => panic!("{}", pair),
+            _ => unreachable!("{}", pair),
         }
     }
 
-    panic!();
+    let o = ast::File {
+        path: (*path).clone(),
+        globals,
+        functions,
+    };
+    eprintln!("Built ast:\n{:#?}", o);
+    Ok(o)
 }
 
-fn parse_fn<'a>(
+fn parse_declare_fn<'a>(
     path: &Arc<PathBuf>,
     pair: Pair<'a, Rule>,
 ) -> Result<ast::DeclareFn<'a>, ParseErr<'a>> {
+    assert!(matches!(pair.as_rule(), Rule::declare_fn));
     let loc = get_loc(path, &pair);
     let mut visibility: HashSet<ast::Visibility> = HashSet::new();
     let mut inner = pair.into_inner();
@@ -70,11 +75,13 @@ fn parse_fn<'a>(
     }
 
     let fullname = t.as_str();
-    let input = parse_declare_data(path.clone(), inner.next().unwrap())?;
+    let input = parse_declare_data(&path, inner.next().unwrap())?;
 
     t = inner.next().unwrap();
     let output = if matches!(t.as_rule(), Rule::declare_data) {
-        Some(parse_declare_data(path.clone(), t)?)
+        let s = Some(parse_declare_data(&path, t)?);
+        t = inner.next().unwrap();
+        s
     } else {
         None
     };
@@ -84,23 +91,92 @@ fn parse_fn<'a>(
         name: fullname,
         input,
         output,
-        block: parse_block(path.clone(), inner.next().unwrap())?,
+        block: parse_block(path, t)?,
         loc,
     })
 }
 
 fn parse_declare_data<'a>(
-    path: Arc<PathBuf>,
+    path: &Arc<PathBuf>,
     pair: Pair<'a, Rule>,
 ) -> Result<ast::DeclareData<'a>, ParseErr<'a>> {
-    panic!();
+    assert!(matches!(pair.as_rule(), Rule::declare_data));
+    let loc = get_loc(path, &pair);
+    let mut data = Vec::new();
+    for pair in pair.into_inner() {
+        assert!(matches!(pair.as_rule(), Rule::declare_data_var));
+        let loc = get_loc(path, &pair);
+        let mut inner = pair.into_inner();
+        let var = expect!(inner.next()).as_str();
+        let type_ = parse_type(path, expect!(inner.next()))?;
+        let assign = if let Some(a) = inner.next() {
+            Some(parse_expr(path, a)?)
+        } else {
+            None
+        };
+        data.push(ast::DeclareDataVar {
+            var,
+            type_,
+            assign,
+            loc,
+        })
+    }
+    Ok(ast::DeclareData { data, loc })
 }
 
 fn parse_block<'a>(
-    path: Arc<PathBuf>,
+    path: &Arc<PathBuf>,
     pair: Pair<'a, Rule>,
 ) -> Result<ast::Block<'a>, ParseErr<'a>> {
     panic!();
+}
+
+fn parse_type<'a>(
+    path: &Arc<PathBuf>,
+    pair: Pair<'a, Rule>,
+) -> Result<ast::Type<'a>, ParseErr<'a>> {
+    assert!(matches!(pair.as_rule(), Rule::type_));
+    let loc = get_loc(path, &pair);
+    let type_inner = pair.into_inner().next().unwrap();
+    assert!(matches!(type_inner.as_rule(), Rule::type_inner));
+    // remove all whitespace
+    let type_str = type_inner
+        .as_str()
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect();
+    Ok(ast::Type {
+        a: ast::AType::Literal(type_str),
+        loc,
+    })
+}
+
+fn parse_expr<'a>(
+    path: &Arc<PathBuf>,
+    pair: Pair<'a, Rule>,
+) -> Result<ast::Expr<'a>, ParseErr<'a>> {
+    assert!(matches!(pair.as_rule(), Rule::expr));
+    let pair = pair.into_inner().next().unwrap();
+    let o = match pair.as_rule() {
+        Rule::value => ast::Expr::Value(parse_value(path, pair)?),
+        _ => panic!("not implemented"),
+    };
+    Ok(o)
+}
+
+
+fn parse_value<'a>(
+    path: &Arc<PathBuf>,
+    pair: Pair<'a, Rule>,
+) -> Result<ast::Value<'a>, ParseErr<'a>> {
+    assert!(matches!(pair.as_rule(), Rule::value));
+    let loc = get_loc(path, &pair);
+    let pair = pair.into_inner().next().unwrap();
+    let a = match pair.as_rule() {
+        Rule::string => ast::AValue::String(pair.as_str()),
+        _ => panic!("not implemented"),
+    };
+    Ok(ast::Value { a, loc })
 }
 
 fn get_loc<'a>(path: &Arc<PathBuf>, pair: &Pair<'a, Rule>) -> ast::Loc<'a> {
