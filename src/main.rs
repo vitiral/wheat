99e334b24863731e558e63bc3ca831fb064a648c
Rett Berg <rett@google.com>
2020-04-21 22:03:29 -0600

    getting close

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate log;
 extern crate env_logger;
 extern crate wast;
 
+#[macro_use]
+extern crate expect_macro;
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
@@ -12,8 +15,8 @@ mod parser;
 
 use std::fs;
 use std::path::PathBuf;
-use structopt::StructOpt;
 use std::sync::Arc;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "lang", about = "An experimental programming language.")]
@@ -34,14 +37,21 @@ fn main() -> Result<(), String> {
     let input = Arc::new(opt.input);
     let text = fs::read_to_string(input.as_ref())
         .map_err(|e| format!("Failed to parse {}: {}", input.display(), e))?;
-    let res = match parser::parse(input.clone(), &text) {
+    let res = match parser::parse(&input, &text) {
         Ok(r) => r,
         Err(e) => {
             eprintln!("Failed to parse {}\n{:#?}", input.display(), e);
             return Err(e.to_string());
         }
     };
+    let file = match parser::build_ast(&input, res) {
+        Ok(r) => r,
+        Err(e) => {
+            let msg = format!("Failed to build ast {}\n{:#?}", input.display(), e);
+            eprintln!("{}", msg);
+            return Err(msg);
+        }
+    };
 
-    println!("Parsed {}:\n{:#?}", input.display(), res);
     Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,21 +1,111 @@
-use pest;
-use pest::Parser;
-use pest::iterators::Pair;
 use crate::ast;
-use std::sync::Arc;
+use pest;
+use pest::iterators::Pair;
+use pest::Parser;
+use std::collections::HashSet;
 use std::path::PathBuf;
+use std::sync::Arc;
 
 #[derive(Parser)]
 #[grammar = "syntax.pest"]
 pub struct LangParser;
 
+#[derive(Debug)]
+pub struct ParseErr<'a> {
+    msg: String,
+    pair: Pair<'a, Rule>,
+}
 
-pub fn parse(path: Arc<PathBuf>, text: &str) -> Result<Vec<Pair<Rule>>, pest::error::Error<Rule>> {
-  let pair = LangParser::parse(Rule::file, text)?.collect();
+impl<'a> ParseErr<'a> {
+    pub fn new(pair: Pair<'a, Rule>, msg: String) -> ParseErr<'a> {
+        ParseErr {
+            msg: msg,
+            pair: pair,
+        }
+    }
+}
 
-  let path = Arc::new(path);
-  // let mut globals = Vec::new();
-  // let mut functions = Vec::new();
+pub fn parse<'a>(
+    path: &Arc<PathBuf>,
+    text: &'a str,
+) -> Result<Vec<Pair<'a, Rule>>, pest::error::Error<Rule>> {
+    Ok(LangParser::parse(Rule::file, text)?.collect())
+}
 
-  Ok(pair)
+pub fn build_ast<'a>(
+    path: &Arc<PathBuf>,
+    pairs: Vec<Pair<'a, Rule>>,
+) -> Result<ast::File<'a>, ParseErr<'a>> {
+    let mut globals: Vec<ast::DeclareVar<'a>> = Vec::new();
+    let mut functions: Vec<ast::DeclareFn<'a>> = Vec::new();
+
+    println!("Parsed {}:\n{:#?}", path.display(), pairs);
+
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::declare_fn => {
+                functions.push(parse_fn(path, pair)?);
+            }
+            Rule::declare_var => {}
+            Rule::EOI => {}
+            _ => panic!("{}", pair),
+        }
+    }
+
+    panic!();
+}
+
+fn parse_fn<'a>(
+    path: &Arc<PathBuf>,
+    pair: Pair<'a, Rule>,
+) -> Result<ast::DeclareFn<'a>, ParseErr<'a>> {
+    let loc = get_loc(path, &pair);
+    let mut visibility: HashSet<ast::Visibility> = HashSet::new();
+    let mut inner = pair.into_inner();
+    let mut t = expect!(inner.next());
+
+    if matches!(t.as_rule(), Rule::VISIBILITY) {
+        visibility.insert(ast::Visibility::new(t.as_str()));
+        t = inner.next().unwrap();
+    }
+
+    let fullname = t.as_str();
+    let input = parse_declare_data(path.clone(), inner.next().unwrap())?;
+
+    t = inner.next().unwrap();
+    let output = if matches!(t.as_rule(), Rule::declare_data) {
+        Some(parse_declare_data(path.clone(), t)?)
+    } else {
+        None
+    };
+
+    Ok(ast::DeclareFn {
+        visibility,
+        name: fullname,
+        input,
+        output,
+        block: parse_block(path.clone(), inner.next().unwrap())?,
+        loc,
+    })
+}
+
+fn parse_declare_data<'a>(
+    path: Arc<PathBuf>,
+    pair: Pair<'a, Rule>,
+) -> Result<ast::DeclareData<'a>, ParseErr<'a>> {
+    panic!();
+}
+
+fn parse_block<'a>(
+    path: Arc<PathBuf>,
+    pair: Pair<'a, Rule>,
+) -> Result<ast::Block<'a>, ParseErr<'a>> {
+    panic!();
+}
+
+fn get_loc<'a>(path: &Arc<PathBuf>, pair: &Pair<'a, Rule>) -> ast::Loc<'a> {
+    return ast::Loc {
+        path: (*path).clone(),
+        span: pair.as_span(),
+    };
 }
