5f48c2b64c5ce17145e7a2fa686ef3f8aa788a97
Rett Berg <rett@google.com>
2020-08-12 21:34:46 -0600

    working on sexpr parser

---
+++ src/sexpr.rs
@@ -0,0 +1,50 @@
+use pest;
+use pest::iterators::Pair;
+use pest::iterators::Pairs;
+use pest::Parser;
+
+#[throws]
+pub fn parse(text: &str) -> S {
+    let exprs: Vec<_> = LangParser::parse(Rule::file, src.text)?
+        .filter(|pair| !matches!(pair.as_rule(), Rule::EOI))
+        .map(parse_s)
+        .collect();
+}
+
+fn parse_expr(pair: Pair<Rule>) -> S {
+    assert!(matches!(pair.as_rule(), Rule::expr), "{}", pair);
+
+    let mut exprs = Vec::new();
+    for pair in pair.into_inner() {
+        let e = match pair.as_rule() {
+            Rule::atom => S::Atom(pair.as_str().to_string()),
+            Rule::sexpr => S::List(parse_s(pair)),
+            Rule::comment => S::Comment(parse_comment(pair)),
+        };
+        exprs.put(e);
+    }
+}
+
+fn parse_s(pair: Pair<Rule>) -> Vec<S> {
+    assert!(matches!(pair.as_rule(), Rule::sexpr), "{}", pair);
+    pair
+        .into_inner()
+        .map(parse_expr)
+        .collect()
+}
+
+fn parse_comment(pair: Pair<Rule>) -> String {
+    assert!(matches!(pair.as_rule(), Rule::comment), "{}", pair);
+    expect!(pair.into_inner().next()).as_str().to_string()
+}
+
+
+#[derive(Parser)]
+#[grammar = "sexpr.pest"]
+pub struct LangParser;
+
+pub enum S {
+    Atom(String),
+    List(Vec<S>),
+    Comment(String),
+}
